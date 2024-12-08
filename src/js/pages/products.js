b0cacea8d00e504e961ccbbac4431613777dbf1e
Elvin <elvinsrkrov@gmail.com>
2024-12-08 18:25:03 +0400

    Fetched all products && filters have added on UI

--- src/js/pages/products.js
+++ src/js/pages/products.js
@@ -1,15 +1,30 @@
 // global imports
-import '../toggleSidebar.js';
+import "../toggleSidebar.js";
 import "../activeNav.js";
-import '../cart/toggleCart.js';
-import '../cart/setupCart.js';
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
 
 //  filter imports
-import setupSearch from '../filters/search.js';
-import setupCompanies from '../filters/companies.js';
-import setupPrice from '../filters/price.js';
+import setupSearch from "../filters/search.js";
+import setupCompanies from "../filters/companies.js";
+import setupPrice from "../filters/price.js";
 
 // specific imports
-import { store } from '../store.js'; 
-import display from '../displayProducts.js';
-import { getElement } from '../utils.js';
+import { store } from "../store.js";
+import display from "../displayProducts.js";
+import { getElement } from "../utils.js";
+
+const loading = getElement(".page-loading");
+const allProductsContainer = getElement(".products-container");
+
+const init = async () => {
+  if (store.length < 1) {
+    const products = await fetchProducts();
+    setupStore(products);
+  }
+  display(store, allProductsContainer);
+
+  loading.style.display = "none";
+};
+
+window.addEventListener("DOMContentLoaded", init);
