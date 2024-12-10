33b9ac44be79cc7a65931f2f86df191023359554
Elvin <elvinsrkrov@gmail.com>
2024-12-10 10:51:12 +0400

    add to cart feature has implemented

--- src/js/cart/setupCart.js
+++ src/js/cart/setupCart.js
@@ -4,9 +4,32 @@ import {
   setStorageItem,
   formatPrice,
   getElement,
-} from '../utils.js';
-import { openCart } from './toggleCart.js';
-import { findProduct } from '../store.js';
-import addToCartDOM from './addToCartDOM.js';
+} from "../utils.js";
+import { openCart } from "./toggleCart.js";
+import { findProduct } from "../store.js";
+import addToCartDOM from "./addToCartDOM.js";
 
-export const addToCart = () => {};
+const cartItemsDOM = getElement(".cart-items");
+let cart = getStorageItem("cart");
+
+export const addToCart = (id) => {
+  let item = cart.find((cartItem) => cartItem.id === id);
+
+  if (!item) {
+    let product = findProduct(id);
+    // add item to the the
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    // add item to the DOM;
+    addToCartDOM(product);
+  } else {
+    // update values
+    const items = [...cartItemsDOM.querySelectorAll(".cart-item-amount")];
+    const newAmount = items.find((value) => value.dataset.id === id);
+    newAmount.textContent = amount;
+  }
+
+  setStorageItem("cart", cart);
+  //more stuff coming up
+  openCart();
+};
